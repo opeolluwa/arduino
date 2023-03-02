cadfcc05555be280fcdbd4fe50e132c6372e88bf
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-03-02 12:57:12 +0100

    initial  commit

---
+++ sketch_mar2b.ino
@@ -0,0 +1,18 @@
+//prelude and lib imports
+
+void setup() {
+  // init
+Serial.begin(9600);
+Serial.println("\"Hello Engineer Hamziey\n\tmy name is Opeoluwa Adeoye I'm a senior fullstack developer\n\tAt Amazon."); 
+Serial.println("Hello, World!");
+  // put your setup code here, to run once:
+  // introduction_to_arduino
+}
+
+void loop() {
+  // looop and exec this block
+
+
+  // put your main code here, to run repeatedly:
+
+}
