6c88511d84ee22338bfa15069026a37f961e30b1
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2023-03-02 13:06:29 +0100

    Working with attributes
    using #if attribute to conditionally compile attribute

--- sketch_mar2b.ino
+++ sketch_mar2b.ino
@@ -1,5 +1,23 @@
 //prelude and lib imports
 
+/**
+* using attribute to determine code compilation
+* the attribute start with `#if 1 (or #if 0)` to mean 
+*if true and if false respectively
+*
+* the syntax go thus
+* #if 0
+*
+* ...
+* my code goes here
+* ....
+*
+* #endif
+*/
+
+// Using truthy and falsy state ment to compile code 
+#if 1
+
 void setup() {
   // init
 Serial.begin(9600);
@@ -16,3 +34,5 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+#endif
